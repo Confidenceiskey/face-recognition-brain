854b0e89e79df6bf5b4300ec9fae62545e509bc1
David N <bigair_1080@hotmail.com>
2021-02-26 17:31:13 +0100

    updated packages & clean up code

--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -42,8 +42,6 @@ class Register extends React.Component {
   }
 
   render () {
-  const { onRouteChange } = this.props;
-
     return (
       <article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
         <main className="pa4 black-80">
