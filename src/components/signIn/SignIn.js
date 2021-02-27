5241da9bda1f62cc23e21cb72492d079c7db5c7a
David N <bigair_1080@hotmail.com>
2021-02-27 12:07:33 +0100

    add origin policy

--- src/components/signIn/SignIn.js
+++ src/components/signIn/SignIn.js
@@ -1,41 +1,44 @@
-import React from 'react';
+import React from "react";
 
 class SignIn extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      signInEmail: '',
-      signInPassword: ''
-    }
+      signInEmail: "",
+      signInPassword: "",
+    };
   }
 
   onEmailChange = (event) => {
-    this.setState({ signInEmail: event.target.value})
-  }
+    this.setState({ signInEmail: event.target.value });
+  };
 
   onPasswordChange = (event) => {
-    this.setState({ signInPassword: event.target.value })
-  }
+    this.setState({ signInPassword: event.target.value });
+  };
 
   onSubmitSignIn = () => {
-    fetch('https://face-brain-dpn.herokuapp.com/signIn', {
-      referrerPolicy: 'origin-when-cross-origin',
-      method: 'post',
-      headers: {'Content-Type': 'application/json'},
-      credentials: 'include',
+    fetch("https://face-brain-dpn.herokuapp.com/signIn", {
+      referrerPolicy: "origin-when-cross-origin",
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "https://smart-brain-dpn.herokuapp.com",
+      },
+      credentials: "include",
       body: JSON.stringify({
         email: this.state.signInEmail,
-        password: this.state.signInPassword
-      })
+        password: this.state.signInPassword,
+      }),
     })
-    .then(response => response.json())
-    .then(data => {
-      if (data.id) {
-        this.props.loadUser(data);
-        this.props.onRouteChange('home');
-      }
-    })
-  }
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.id) {
+          this.props.loadUser(data);
+          this.props.onRouteChange("home");
+        }
+      });
+  };
 
   render() {
     const { onRouteChange } = this.props;
@@ -46,36 +49,46 @@ class SignIn extends React.Component {
             <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
               <legend className="f2 fw6 ph0 mh0">Sign In</legend>
               <div className="mt3">
-                <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-                <input 
-                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
-                  type="email" 
-                  name="email-address"  
+                <label className="db fw6 lh-copy f6" htmlFor="email-address">
+                  Email
+                </label>
+                <input
+                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                  type="email"
+                  name="email-address"
                   id="email-address"
                   onChange={this.onEmailChange}
                 />
               </div>
               <div className="mv3">
-                <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-                <input 
-                  className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
-                  type="password" 
-                  name="password"  
+                <label className="db fw6 lh-copy f6" htmlFor="password">
+                  Password
+                </label>
+                <input
+                  className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                  type="password"
+                  name="password"
                   id="password"
                   onChange={this.onPasswordChange}
                 />
               </div>
             </fieldset>
             <div className="">
-              <input 
+              <input
                 onClick={this.onSubmitSignIn}
-                className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
-                type="submit" 
+                className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
+                type="submit"
                 value="Sign in"
               />
             </div>
             <div className="lh-copy mt3">
-              <p onClick={() => onRouteChange('register')} href="#0" className="f6 link dim black db pointer">Register</p>
+              <p
+                onClick={() => onRouteChange("register")}
+                href="#0"
+                className="f6 link dim black db pointer"
+              >
+                Register
+              </p>
             </div>
           </div>
         </main>
